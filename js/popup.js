b80f1a7418febfe6dc37b7fd3e211ce2ab4a50ab
AndrewChanse <andryccio@gmail.com>
2017-10-10 21:30:42 +0300

    popup script added

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+var link = document.querySelector(".login");
+var popup = document.querySelector(".modal-content");
+var close = document.querySelector(".modal-content-close");
+
+link.addEventListener("click", function(event) {
+ event.preventDefault();
+  popup.classList.add("modal-content-view");
+ });
+
+close.addEventListener("click", function(event) {
+ event.preventDefault();
+  popup.classList.remove("modal-content-view");
+});
